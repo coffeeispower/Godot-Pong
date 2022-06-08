883f64ebb5255c804de304ab3041869783d3014f
tiagodinis33 <tiagodinis33@gmail.com>
2022-06-08 11:43:26 +0100

    Adds ball movement

--- Ball.gd
+++ Ball.gd
@@ -1,6 +1,10 @@
 extends KinematicBody2D
-
-
+export(int) var speed = 0;
+export var direction = Vector2(-1, 1)
 func _process(delta):
+	move_and_slide(direction*speed, Vector2(0, -1))
+	if(is_on_ceiling() || is_on_floor()):
+		direction.y = -direction.y
+	if(is_on_wall()):
+		direction.x = -direction.x
 	
-	pass
